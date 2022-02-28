c36879806b26aa153eb7c3425e221b83502d787c
Tony <mr.landero@gmail.com>
2022-02-28 12:40:07 -0600

    Create multipage Streamlit outline

---
+++ apps/buyer.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def buyer():
+  st.markdown("## Welcome to the Buyer's Area")
+  st.write("View our current NFT's for sale")
---
+++ apps/creator.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def creator():
+  st.markdown("## Welcome to the Creator's Section")
+  st.write("Here, you will be able to mint and sell your NFT's")
